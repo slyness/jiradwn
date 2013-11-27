24bb4320549a2d8473b9b72c58942aacbf80d203
Aaron Baer <aaron@hw-ops.com>
2013-11-27 13:45:16 -0800

    setup downloading

--- lib/jiradwn/downloader.rb
+++ lib/jiradwn/downloader.rb
@@ -1,13 +1,40 @@
+require 'fileutils'
+
 module Jiradwn
   class Downloader
 
-    attr_reader :user, :password, :storage, :endpoint
-
-    def initialize()
+    def initialize(global,options)
+      @user = global[:u]
+      @pass = global[:p]
+      @endpoint = options[:e]
+      @storage = options[:s]
+      @today = Time.new
     end
 
-    def download_backup()
-      puts "Hello World"
+    def download_backup
+      puts "Downloading file from " + @endpoint
+
+      backupfile = "JIRA-backup-" + @today.strftime("%Y%m%d") + ".zip"
+      uri = "https://" + @endpoint + "/webdav/backupmanager/" + backupfile
+      location = File.join(@storage,backupfile)
+      command = "wget --user=#{@user} --password=#{@pass} -c #{uri} -O #{location}"
+
+      FileUtils.mkdir_p(@storage)
+      system(%(#{command}))
+
+      puts command
+      puts "#{uri}"
+      puts location
+    end
+
+    def backup_exists
+      return true
+    end
+
+    def run!
+      if backup_exists
+        download_backup
+      end
     end
 
   end
