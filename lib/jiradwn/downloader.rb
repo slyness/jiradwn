3c6c4e15b53f4628f6a619a05cbf95e014add18c
Aaron Baer <aaron@hw-ops.com>
2013-12-05 17:47:50 -0800

    make sure downloader is using net/http as well

--- lib/jiradwn/downloader.rb
+++ lib/jiradwn/downloader.rb
@@ -1,4 +1,6 @@
 require 'fileutils'
+require 'net/http'
+require 'uri'
 
 module Jiradwn
   class Downloader
@@ -13,25 +15,48 @@ module Jiradwn
     end
 
     def download_backup
-      puts "Downloading file from " + @endpoint
+
+      location = File.join(@storage,@backupfile)
+
+      unless File.directory?(@storage)
+        FileUtils.mkdir_p(@storage)
+      end
 
       uri = "https://" + @endpoint + "/webdav/backupmanager/" + @backupfile
-      location = File.join(@storage,@backupfile)
-      command = "wget --user=#{@user} --password=#{@pass} -c #{uri} -O #{location}"
+      uri = URI.parse(uri)
+      puts "Downloading: #{uri}"
+      puts "To: #{location}"
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      begin
+        http.start do |dwn|
+          request = Net::HTTP::Get.new(uri.request_uri)
+          request.basic_auth(@user, @pass)
+          response = dwn.request(request)
+          case response.code
+          when "200"
+            dwn.request(request) do |response|
+              File.open(location, 'w') do |incoming|
+                response.read_body do |chunk|
+                  incoming.write(chunk)
+                end
+              end
+            end
+            puts "Download Complete"
+          when "401"
+            puts "Authorization error. Please check your username and password and try again."
+          end
+        end
+      rescue Exception => e
+        puts "Skipping download. This happened: #{e}"
+        return
+      end
 
-      FileUtils.mkdir_p(@storage)
-      system(%(#{command}))
-
-    end
-
-    def backup_exists
-      return true
     end
 
     def run!
-      if backup_exists
         download_backup
-      end
     end
 
   end
